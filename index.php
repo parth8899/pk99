3648e5cbfc105fe3cd9e3b0d1f7963a9281afa24
parth kamani <parthkamani21@gmail.com>
2017-03-11 15:56:47 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+require 'PHPMailer/PHPMailerAutoload.php';
+
+$mail = new PHPMailer;
+
+$mail->isSMTP();                                   // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';                    // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                            // Enable SMTP authentication
+$mail->Username = 'Insert Email Address';          // SMTP username
+$mail->Password = 'Insert Email Account Password'; // SMTP password
+$mail->SMTPSecure = 'tls';                         // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                 // TCP port to connect to
+
+$mail->setFrom('email@codexworld.com', 'CodexWorld');
+$mail->addReplyTo('email@codexworld.com', 'CodexWorld');
+$mail->addAddress('john@gmail.com');   // Add a recipient
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+
+$mail->isHTML(true);  // Set email format to HTML
+
+$bodyContent = '<h1>How to Send Email using PHP in Localhost by CodexWorld</h1>';
+$bodyContent .= '<p>This is the HTML email sent from localhost using PHP script by <b>CodexWorld</b></p>';
+
+$mail->Subject = 'Email from Localhost by CodexWorld';
+$mail->Body    = $bodyContent;
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
+?>
